3f0db18ed47ca7c6571a0b3f14dabadafe8c1669
LYM010 <klsmuej@126.com>
2018-07-26 18:01:21 +0800

    test62.py

---
+++ test62.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+'''
+题目：查找字符串。　　
+
+程序分析：无。
+'''
+sStr1 = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
+sStr2 = 'c'
+print(sStr1.find(sStr2))

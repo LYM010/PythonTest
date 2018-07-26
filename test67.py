bcceee23323e32ea5e312de66c28fdfe0bf65027
LYM010 <klsmuej@126.com>
2018-07-26 18:20:49 +0800

    test67.py

---
+++ test67.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+'''
+题目：输入数组，最大的与第一个元素交换，最小的与最后一个元素交换，输出数组。
+
+程序分析：无。
+'''
+import random
+l = [random.randint(0,100) for i in range(10)]
+print(l)

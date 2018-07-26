bd953dba052ba1f5acd25ee9884ecad0eb67096a
LYM010 <klsmuej@126.com>
2018-07-26 18:56:16 +0800

    test67.py

--- test67.py
+++ test67.py
@@ -6,5 +6,28 @@
 程序分析：无。
 '''
 import random
-l = [random.randint(0,100) for i in range(10)]
-print(l)
+
+def arr_max(array=[]):
+	max = array[0]
+	flag = 0
+	for i in range(1,len(array)):
+		if max < array[i]:
+			max = array[i]
+			flag = i
+	array[0],array[flag] = array[flag],array[0]
+	return array
+
+def arr_min(array=[]):
+	min = array[0]
+	flag = 0
+	for i in range(1,len(array)):
+		if min > array[i]:
+			min = array[i]
+			flag = i
+	array[-1],array[flag] = array[flag],array[-1]
+	return array
+
+if __name__ == '__main__':
+	l = [random.randint(0,100) for i in range(10)]
+	print(l)
+	print(arr_min(arr_max(l)))

b95e07c688dbe1dfed022b5f14b1505c47840a6b
LYM010 <klsmuej@126.com>
2018-07-26 23:08:18 +0800

    test86.py

---
+++ test86.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+'''
+题目：两个字符串连接程序。
+程序分析：无。
+'''
+if __name__ == '__main__':
+    a = "acegikm"
+    b = "bdfhjlnpq"
+ 
+    # 连接字符串
+    c = a + b
+    print(c)

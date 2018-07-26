2a47c6d9b957007107b456624feb3107bd470667
LYM010 <klsmuej@126.com>
2018-07-26 21:02:56 +0800

    test78.py

---
+++ test78.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+'''
+题目：找到年龄最大的人，并输出。请找出程序中有什么问题。
+程序分析：无。
+'''
+if __name__ == '__main__':
+    person = {"li":18,"wang":50,"zhang":20,"sun":22}
+    m = 'li'
+    for key in person.keys():
+        if person[m] < person[key]:
+            m = key
+ 
+    print('{} {}'.format(m,person[m]))

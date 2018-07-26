84b7dfbfd0ae24a6fbc48837eeb4543c1c0a327b
LYM010 <klsmuej@126.com>
2018-07-26 12:14:42 +0800

    test22.py

--- test16.py
+++ test16.py
@@ -1,18 +1,1 @@
-#!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
-'''
-题目：输出指定格式的日期。
-程序分析：使用 datetime 模块。
-'''
-import time
-
-print(time.time())
-print(time.localtime())
-print(time.asctime())
-print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())) 
-
-import datetime
-
-print(datetime.date.today()) 
-print(datetime.date.today().strftime('%d/%m/%Y'))
-print(datetime.date(1941, 1, 5))
+#!/usr/bin/env python3# -*- coding: UTF-8 -*-'''程序分析：使用 datetime 模块。题目：输出指定格式的日期。'''import datetimeimport timeprint(datetime.date(1941, 1, 5))print(datetime.date.today())print(datetime.date.today().strftime('%d/%m/%Y'))print(time.asctime())print(time.localtime())print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))print(time.time())

eef92405817922996111520f20a8eea7cbf89aad
LYM010 <klsmuej@126.com>
2018-07-27 12:51:30 +0800

    collatz.py

--- collatz.py
+++ collatz.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
-# -*-coding:UTF-8-*-
-def collatz(num):
-    print(num)
-    if num == 1:
-        return "succeed!"
-    elif num%2 == 0:
-        collatz(n = num//2)
-    else:
-        collatz(num*3+1)
+# -*- coding: UTF-8 -*-
+'''
+题目：时间函数举例3。
+程序分析：无。
+'''
 
-if __name__=='__main__':
-    n = int(input('Enter number:')
-    print(collatz(n))
+def f(num):
+	print(num)
+	if num == 1:
+		return
+	elif num%2 == 0:
+		f(num//2)
+	elif num%2 == 1:
+		f(num*3+1)
+
+if __name__ == '__main__':
+    n = int(input('Enter a number:'))
+    f(n)

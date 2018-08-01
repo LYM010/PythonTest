1af476cca82e88f3dedfce1c67111287b17080ec
LYM010 <klsmuej@126.com>
2018-08-01 23:45:42 +0800

    mapIt.py

---
+++ mapIt.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import webbrowser,sys,logging,pyperclip
+logging.basicConfig(level = logging.DEBUG,
+	format='%(asctime)s - %(levelname)s - %(message)s')
+logging.disable(logging.CRITICAL)
+
+if len(sys.argv) > 1:
+	address = ''.join(sys.argv[1:])
+	logging.debug(f'address_argv is {address}')
+else:
+	address = pyperclip.paste()
+	logging.debug(f'address_pyperclip is {address}')
+
+webbrowser.open(f'http://api.map.baidu.com/geocoder?address={address}&output=html&src=test')

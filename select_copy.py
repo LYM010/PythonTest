eb92804c893e6f6a96dccfbcb168355c98905387
LYM010 <klsmuej@126.com>
2018-08-01 11:31:19 +0800

    select_copy.py

---
+++ select_copy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*-UTF8-*-
+# 编写一个程序，遍历一个目录树，查找特定扩展名的文件（诸如.pdf 或.jpg），
+# 不论这些文件的位置在哪里， 将它们拷贝到一个新的文件夹中。
+
+import os,shutil,re
+path =os.path.abspath( '..\\tmp')
+folder = os.path.abspath('..\\test')
+
+if os.path.exists(folder):
+    print('Folder exists.')
+else:
+    os.mkdir(folder)
+    print('Folder created successfuly!')
+
+type = 'txt'
+
+for root,dirs,files in os.walk(path):
+    for fileName in files:
+        mo = re.compile(f'.*\.{type}$').search(fileName)
+        if mo == None:
+            continue
+        else:
+            fillName = os.path.join(root,mo.group())
+            print(fillName)
+            shutil.copy(fillName,folder)

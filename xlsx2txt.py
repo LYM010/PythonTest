59ca6e52fa87e81a5518ba909d3c0d97719c3ade
LYM010 <klsmuej@126.com>
2018-08-03 21:05:20 +0800

    xlsx2txt.py

--- xlsx2txt.py
+++ xlsx2txt.py
@@ -11,7 +11,10 @@ wb = openpyxl.load_workbook('example.xlsx')
 sheet = wb['Sheet1']
 logging.debug(f'sheet is {sheet}')
 
+logging.debug('Write in files...')
 for columnNum in range(1,sheet.max_column+1):
 	with open(f'xlsx{columnNum}.txt','w') as file:
+		logging.debug(f'Write in xlsx{columnNum}.txt')
 		for rowNum in range(1,sheet.max_row+1):
 			file.write(str(sheet.cell(row=rowNum,column=columnNum).value)+'\r\n')
+logging.debug('Done.')
